c72ba6aa09019bbc0cf94ce6265df2c433a7f929
EnikoKancsar <enikokancsar@gmail.com>
2018-04-23 20:52:24 +0200

    IsInList

---
+++ week-02/day-3/Lists/src/IsInList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class IsInList{
+    public static void main(String... args){
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+        System.out.println(checkNums(list));
+    }
+
+    public static boolean checkNums(ArrayList<Integer> list) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(4, 8, 12, 16));
+        return list.containsAll(numbers);
+    }
+}
