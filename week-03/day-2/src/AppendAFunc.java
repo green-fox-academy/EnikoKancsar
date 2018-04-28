5b32eb03bb7a557eaf00704f7627f9cdcd338881
EnikoKancsar <enikokancsar@gmail.com>
2018-04-28 11:47:30 +0200

    AppendAFunc

---
+++ week-03/day-2/src/AppendAFunc.java
@@ -0,0 +1,10 @@
+public class AppendAFunc {
+    public static void main(String[] args) {
+        String am = "kuty";
+        System.out.println(appendAFunc(am));
+    }
+
+    public static String appendAFunc(String am) {
+        return am + "a";
+    }
+}
