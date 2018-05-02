62398412cec4a3ed3ff7b7a07239f2affcf150c5
EnikoKancsar <enikokancsar@gmail.com>
2018-05-02 17:51:50 +0200

    AppendA

---
+++ week-03/day-2/src/AppendA.java
@@ -0,0 +1,8 @@
+public class AppendA {
+    public static void main(String[] args) {
+        String[] animals = {"kuty", "macsk", "cic"};
+        for (int i = 0; i < animals.length; i++) {
+            animals[i] += "a";
+        }
+    }
+}
