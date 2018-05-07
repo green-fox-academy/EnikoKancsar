8a11976c4f4328ab580d3cebe9c064425337445c
EnikoKancsar <enikokancsar@gmail.com>
2018-05-07 18:07:23 +0200

    Teachers and students

---
+++ week-05/day-1/teachers-and-students/src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public void learn() {
+        System.out.println("He?");
+    }
+
+    public void ask(Teacher teacher) {
+        System.out.println("Miért?");
+        teacher.answer();
+    }
+}
---
+++ week-05/day-1/teachers-and-students/src/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+    public void teach(Student student) {
+        System.out.println("A posztindusztriális hermeneutika forradalma");
+        // http://www.dragonweb.hu/files/thesis-bullshit-generator.html
+        student.learn();
+    }
+
+    public void answer() {
+        System.out.println("Azért mert csak.");
+    }
+}
