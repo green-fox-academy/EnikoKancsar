1e3b21d8ebfcc58dd3a711add51068b43f083b8f
EnikoKancsar <enikokancsar@gmail.com>
2018-05-07 19:17:18 +0200

    SharpieSet constructor added

--- week-05/day-1/SharpieSet/src/SharpieSet.java
+++ week-05/day-1/SharpieSet/src/SharpieSet.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class SharpieSet {
     private ArrayList<Sharpie> sharpieSet;
 
+    public SharpieSet() {
+        sharpieSet = new ArrayList<>();
+    }
+
     public int countUsable() {
         int counter = 0;
         for (Sharpie currentSharpie : sharpieSet) {
