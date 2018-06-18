ba6f6d0ecc60903ed67bf2172c98d1e09f353640
EnikoKancsar <enikokancsar@gmail.com>
2018-06-18 20:42:57 +0200

    New Bank Of Simba T.T

---
+++ week-10/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/WebController.java
@@ -0,0 +1,48 @@
+package com.greenfoxacademy.bankofsimba.controller;
+
+import com.greenfoxacademy.bankofsimba.model.BankAccount;
+import com.greenfoxacademy.bankofsimba.service.BankAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    private final
+    BankAccountService bankService;
+
+    @Autowired
+    public WebController(BankAccountService bankService) {
+        this.bankService = bankService;
+    }
+
+    @GetMapping("show")
+    public String getSimba(Model thymeLeafModel) {
+        BankAccount simba = new BankAccount("Simba", 2000.00, "lion");
+        bankService.add(simba);
+        thymeLeafModel.addAttribute("bankAccounts", bankService.getAllAccount());
+        return "accountSimba";
+    }
+
+    @GetMapping("showAll")
+    public String getAllAccount(Model thymeLeafModel) {
+        BankAccount simba = new BankAccount("Simba", 2000.00, "lion");
+        bankService.add(simba);
+        BankAccount nala = new BankAccount("Nala", 2000.00, "lion");
+        bankService.add(nala);
+        BankAccount zazu = new BankAccount("Zazu", 2000.00, "red-bild hornbill");
+        bankService.add(zazu);
+        BankAccount rafiki = new BankAccount("Rafiki", 2000.00, "mandrill");
+        bankService.add(rafiki);
+        thymeLeafModel.addAttribute("bankAccounts", bankService.getAllAccount());
+        return "allAccounts";
+    }
+
+    @GetMapping("HTMLception")
+    public String getHTML(Model thymeLeafModel) {
+        thymeLeafModel.addAttribute("htmlText", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+        return "htmlception";
+    }
+}
---
+++ week-10/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/service/BankAccountService.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.bankofsimba.service;
+
+import com.greenfoxacademy.bankofsimba.model.BankAccount;
+
+import java.util.ArrayList;
+
+public interface BankAccountService {
+    void add(BankAccount bankAccount);
+
+    ArrayList<BankAccount> getAllAccount();
+}
