7488686afdbf72572fcf0d8542bf30d16ddf9098
EnikoKancsar <enikokancsar@gmail.com>
2018-06-27 19:30:00 +0200

    commitcommit

--- week-11/day-1/hellobeanworldapplication/src/main/java/com/greenfoxacademy/hellobeanworldapplication/HellobeanworldapplicationApplication.java
+++ week-11/day-1/hellobeanworldapplication/src/main/java/com/greenfoxacademy/hellobeanworldapplication/HellobeanworldapplicationApplication.java
@@ -1,22 +1,28 @@
 package com.greenfoxacademy.hellobeanworldapplication;
 
 import com.greenfoxacademy.hellobeanworldapplication.services.Printer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class HellobeanworldapplicationApplication implements CommandLineRunner {
 
-    @Autowired
-    Printer printer;
+    private static final Logger logger = LoggerFactory.getLogger(HellobeanworldapplicationApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(HellobeanworldapplicationApplication.class, args);
+        SpringApplication.run(HellobeanworldapplicationApplication.class, args)
+                .getBean(Printer.class)
+                .log("hello");
     }
 
     @Override
     public void run(String... args) throws Exception {
-        printer.log("hello");
+        logger.info("Application started");
     }
 }
--- week-11/day-1/hellobeanworldapplication/src/main/java/com/greenfoxacademy/hellobeanworldapplication/services/Printer.java
+++ week-11/day-1/hellobeanworldapplication/src/main/java/com/greenfoxacademy/hellobeanworldapplication/services/Printer.java
@@ -6,9 +6,6 @@ import java.time.LocalDateTime;
 
 @Service
 public class Printer {
-    public Printer() {
-        System.out.println(LocalDateTime.now() + "letrejottem");
-    }
     public void log(String message) {
         System.out.println(LocalDateTime.now() + " MY PRINTER SAYS --- " + message);
     }
