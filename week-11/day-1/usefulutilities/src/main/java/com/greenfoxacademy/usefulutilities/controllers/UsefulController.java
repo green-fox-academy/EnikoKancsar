740daaa60e09940124395a5c221787d52c5d749b
EnikoKancsar <enikokancsar@gmail.com>
2018-07-04 20:14:43 +0200

    add(email validator)

--- week-11/day-1/usefulutilities/src/main/java/com/greenfoxacademy/usefulutilities/controllers/UsefulController.java
+++ week-11/day-1/usefulutilities/src/main/java/com/greenfoxacademy/usefulutilities/controllers/UsefulController.java
@@ -1,14 +1,24 @@
 package com.greenfoxacademy.usefulutilities.controllers;
 
+import com.greenfoxacademy.usefulutilities.services.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.awt.*;
 import java.util.Random;
 
 @Controller
 public class UsefulController {
+
+    UtilityService utilityService;
+
+    @Autowired
+    public UsefulController(UtilityService utilityService) {
+        this.utilityService = utilityService;
+    }
     @GetMapping("useful")
     public String loadMainPage() {
         return "mainpage";
@@ -22,5 +32,13 @@ public class UsefulController {
         int b = rand.nextInt(256);
         model.addAttribute("randomColor", "rgb(" + r + ", " + g + ", " + b + ")");
         return "randomColorPage";
+    }
+
+    @RequestMapping("useful/emailValidator")
+    public String emailCheck(@RequestParam String emailAddress, Model model) {
+        model.addAttribute("isValid", utilityService.validateEmail(emailAddress));
+        model.addAttribute("validEmail", emailAddress + " is a valid email address");
+        model.addAttribute("invalidEmail", emailAddress + " is not a valid email address");
+        return "emailValidator";
     }
 }
