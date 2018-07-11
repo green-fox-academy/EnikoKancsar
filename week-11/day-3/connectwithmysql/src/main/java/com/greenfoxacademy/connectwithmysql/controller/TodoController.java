1c56421fdf6ffbf16503b76453e117b202f23641
EnikoKancsar <enikokancsar@gmail.com>
2018-07-11 20:09:29 +0200

    Creating todos

--- week-11/day-3/connectwithmysql/src/main/java/com/greenfoxacademy/connectwithmysql/controller/TodoController.java
+++ week-11/day-3/connectwithmysql/src/main/java/com/greenfoxacademy/connectwithmysql/controller/TodoController.java
@@ -1,10 +1,14 @@
 package com.greenfoxacademy.connectwithmysql.controller;
 
+import com.greenfoxacademy.connectwithmysql.model.Todo;
 import com.greenfoxacademy.connectwithmysql.repository.TodoRepository;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -18,5 +22,17 @@ public class TodoController {
         model.addAttribute("todos", todoRepository.findAll());
         model.addAttribute("isActive", isActive);
         return "todolist";
+    }
+
+    @GetMapping("list/add")
+    public String addTodo(Model model) {
+        model.addAttribute("newTodo", new Todo());
+        return "addtodo";
+    }
+
+    @PostMapping("list/add")
+    public String saveTodo(@ModelAttribute Todo newTodo) {
+        todoRepository.save(newTodo);
+        return "redirect:";
     }
 }
