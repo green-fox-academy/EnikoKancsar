2720a7ddb35c3a478d146325f66a3051cd6901e7
EnikoKancsar <enikokancsar@gmail.com>
2018-07-12 10:57:17 +0200

    add(delete)

--- week-11/day-3/connectwithmysql/src/main/java/com/greenfoxacademy/connectwithmysql/controller/TodoController.java
+++ week-11/day-3/connectwithmysql/src/main/java/com/greenfoxacademy/connectwithmysql/controller/TodoController.java
@@ -6,10 +6,7 @@ import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TodoController {
@@ -34,5 +31,11 @@ public class TodoController {
     public String saveTodo(@ModelAttribute Todo newTodo) {
         todoRepository.save(newTodo);
         return "redirect:";
+    }
+
+    @GetMapping("list/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        todoRepository.deleteById(id);
+        return "redirect:http://localhost:8080/list";
     }
 }
